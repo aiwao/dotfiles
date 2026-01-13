92d3cca52e683bf054b259035a06e19ce50e1568
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-13 21:38:00 +0900

    init

---
+++ nvim/init.lua
@@ -0,0 +1,56 @@
+--Replace leader
+vim.g.mapleader = " "
+
+--Set key maps
+vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>")
+
+--Device clipboard
+vim.o.clipboard = "unnamedplus"
+
+--Indent options
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+--Indent options by filetypes
+local filetype_tabstop = {
+	lua=2,
+	markdown=2,
+	html=2,
+}
+--false=Use tab
+local filetype_expand = {
+	go=false,
+}
+--Set indent options for all languages
+local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true})
+vim.api.nvim_create_autocmd("FileType", {
+  group = usrftcfg,
+  callback = function (args)
+		local fte = filetype_expand[args.match]
+		if fte then
+			vim.bo.expandtab = fte
+		end
+
+    local ftts = filetype_tabstop[args.match]
+    if ftts then
+      vim.bo.tabstop = ftts
+      vim.bo.shiftwidth = ftts
+    end
+  end
+})
+
+--Lazy.nvim
+require("config.lazy")
+
+--Mason lsp
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers({
+  function (server_name)
+    require("lspconfig")[server_name].setup({})
+  end,
+})
+
+vim.diagnostic.config()
