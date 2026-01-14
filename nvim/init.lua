01812acc10e3658a370450fc974aa865f8bab4b5
aa <aa@macbook.local>
2026-01-14 12:49:20 +0900

    syntax highlight

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,8 +58,14 @@ require("mason-lspconfig").setup_handlers({
   end,
 })
 
+--Treesitter
 require("nvim-treesitter").setup({
-  install_dir = vim.fn.stdpath("data") .. "/site",
+  install_dir = vim.fn.stdpath('data') .. '/site',
+})
+require("nvim-treesitter").install({ "go", })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
 })
 
 vim.diagnostic.config()
