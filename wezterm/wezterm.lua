436ca58d66cc9f81a310615632bf217e41d685c6
aa <aa@macbook.local>
2026-01-14 18:28:51 +0900

    a

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,16 @@ local wezterm = require("wezterm")
 local keys = require("keymaps")
 
 local config = {
+  color_scheme = "Tokyo Night",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 13.0,
   use_ime = true,
   keys = keys,
 }
+
+wezterm.on('gui-startup', function()
+    local _, _, window = wezterm.mux.spawn_window({})
+    window:gui_window():toggle_fullscreen()
+end)
 
 return config
