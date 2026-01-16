360472480692cdbeab3c8247ddc8e4fca5087d38
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-17 00:39:27 +0900

    spectre

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,18 @@
 --Nvim tree
 vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { silent = true })
 vim.keymap.set("n", "<leader>to", "<cmd>NvimTreeOpen<CR>", { silent = true })
+
 --Bufferline
 vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bc", "<cmd>bdelete<CR>", { silent = true })
+
 --Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", { silent = true })
+--vim.keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", { silent = true })
+
+--Spectre
+vim.keymap.set("n", "<leader>ff", '<cmd>lua require("spectre").toggle()<CR>')
+vim.keymap.set("n", "<leader>fg", '<cmd>lua require("spectre").open_visual({ select_word = true })<CR>')
 
 --Global
 vim.keymap.set("n", "<leader>zz", vim.diagnostic.open_float)
