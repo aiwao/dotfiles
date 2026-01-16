b4da78e24a9f7a2f48aa0971ea02390270fd26ed
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-16 14:36:56 +0900

    bufferline keybind

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 --Nvim tree
 vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { silent = true })
 vim.keymap.set("n", "<leader>to", "<cmd>NvimTreeOpen<CR>", { silent = true })
+--Bufferline
+vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>", { silent = true })
