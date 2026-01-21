4cbf7f683362eba36585d48b4b0824e0017fb8cc
aiwao <233898056+aiwao@users.noreply.github.com>
2026-01-21 19:47:12 +0900

    nvim-treesitter config fix

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,40 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
+  event = { "BufReadPost", "VeryLazy" },
   build = ":TSUpdate",
   main = "nvim-treesitter.config",
   config = function()
+    local languages = {
+        "go",
+        "dockerfile",
+        "dart",
+        "ecma",
+        "sql",
+        "rust",
+        "typescript",
+        "javascript",
+        "html",
+        "html_tags",
+        "css",
+        "svelte",
+        "tsx",
+        "jsx",
+    }
+
     require("nvim-treesitter.config").setup({
+      ensure_installed = languages,
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+    })
+    
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = languages,
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+      end,
     })
   end,
 }
