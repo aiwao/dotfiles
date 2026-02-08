f1fe9b6593ac834441ddba39edb7e262154a0e0f
aiwao <233898056+aiwao@users.noreply.github.com>
2026-02-08 17:42:03 +0900

    fix(fish): fix ghq-c function, invalid define orders, invalid git remote command

--- fish/functions/ghq-c.fish
+++ fish/functions/ghq-c.fish
@@ -4,20 +4,19 @@ function ghq-c --description "Create a ghq directory and a GitHub repo, then cd 
         return 1
     end
 
+    set -l project $argv[1]
     set -l git_username (git config --get user.name); or return 1
     set -l ghq_root (ghq root); or return 1
     set -l repository github.com/$git_username/$project
-    set -l ghq_dir $ghq_root/$repository
-    set -l project $argv[1]
 
     ghq create $project
     or return 1
     gh repo create $argv
     or begin
-        rm -rf $ghq_dir
+        ghq rm $project
         return 1
     end
 
-    cd $ghq_dir
-    git remote add origin https://$reposityory.git
+    cd $ghq_root/$repository
+    git remote add origin main https://$reposityory.git
 end
