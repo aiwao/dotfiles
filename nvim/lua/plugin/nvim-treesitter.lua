553864f3582911cf41c4f594d599f14280463382
aiwao <233898056+aiwao@users.noreply.github.com>
2026-02-09 15:16:03 +0900

    fix(nvim): use group in treesitter autocmd

--- nvim/lua/plugin/nvim-treesitter.lua
+++ nvim/lua/plugin/nvim-treesitter.lua
@@ -1,7 +1,9 @@
 --nvim-treesitter was added by nix @nix/modules/home/programs/neovim.nix
 require("nvim-treesitter").setup()
 
+local treesittergroup = vim.api.nvim_create_augroup("TreesitterGroup", { clear = true})
 vim.api.nvim_create_autocmd("FileType", {
+  group = treesittergroup,
   pattern = "*",
   callback = function(ctx)
     pcall(vim.treesitter.start)
