2c1aa62982486c165f889af13bb0399862db2e54
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-09 17:48:53 +0900

    feat(nvim): customize catppuccin

--- nvim/lua/plugin/catppuccin.lua
+++ nvim/lua/plugin/catppuccin.lua
@@ -1,5 +1,11 @@
 vim.pack.add { "https://github.com/catppuccin/nvim" }
 require("catppuccin").setup {
   flavour = "mocha",
+  custom_highlights = function(colors)
+    return {
+      LineNr = { fg = colors.flamingo, style = { "bold" } },
+      CursorLineNr = { fg = colors.sappire, style = { "bold" } },
+    }
+  end
 }
 vim.cmd.colorscheme("catppuccin")
