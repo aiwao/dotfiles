f7861dacb153e56ccbc3e7c38bdb1988522514d9
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-11 18:59:43 +0900

    fix(nvim): fix dashboard terminal, dont launch terminal when launch neovim abnormally

--- nvim/lua/config/dashboard.lua
+++ nvim/lua/config/dashboard.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.cmd("terminal")
+  callback = function(args)
+    if vim.api.nvim_buf_get_name(args.buf) == "" then
+      vim.opt_local.number = false
+      vim.opt_local.relativenumber = false
+      vim.cmd("terminal")
+    end
   end,
 })
