b1412059dacd4bd5c132da2fb1657df15f38a2bb
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-19 16:37:08 +0900

    neovim:keybind: Redo was bound to `U`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,8 @@
 local Oil = require("oil")
 vim.keymap.set("n", "<leader>tt", function() Oil.open_float() end, { silent = true })
 
---Snacks
 local Snacks = require("snacks")
+
 --Find
 vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end)
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end)
@@ -12,6 +12,7 @@ vim.keymap.set("n", "<leader>fg", function() Snacks.picker.grep() end)
 --LSP
 vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end)
 vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end)
+
 --Buffer
 vim.keymap.set("n", "<leader>bd", function() Snacks.bufdelete() end)
 vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { silent = true })
@@ -20,8 +21,12 @@ vim.keymap.set("n", "<leader>bm", "<cmd>BufferLinePick<CR>", { silent = true })
 
 --Diagnostic
 vim.keymap.set("n", "<leader>zz", vim.diagnostic.open_float)
+
 --Terminal
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+
+--Edit
+vim.keymap.set("n", "U", "<C-r>", { noremap = true, silent = true })
 
 --Disable
 vim.keymap.set({ "n", "i", "v", "x", "o"}, "<PageUp>", "<Nop>", { silent = true })
