e719014d7f76ef41c4eebffb6d06d420727fe143
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-19 16:35:23 +0900

    neovim:keybind: `Snacks.picker.grep()` was re-bound from `<leader>gr` to `<leader>fg`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,8 @@ local Snacks = require("snacks")
 --Find
 vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end)
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end)
+vim.keymap.set("n", "<leader>fg", function() Snacks.picker.grep() end)
+
 --LSP
 vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end)
 vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end)
@@ -15,9 +17,6 @@ vim.keymap.set("n", "<leader>bd", function() Snacks.bufdelete() end)
 vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bb", "<cmd>BufferLineCyclePrev<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bm", "<cmd>BufferLinePick<CR>", { silent = true })
-
---Grep
-vim.keymap.set("n", "<leader>gr", function() Snacks.picker.grep() end)
 
 --Diagnostic
 vim.keymap.set("n", "<leader>zz", vim.diagnostic.open_float)
