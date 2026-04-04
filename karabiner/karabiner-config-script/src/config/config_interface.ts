36c40dd2329d83f576d0d4e014a07cdc6d6e9cd4
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-05 00:37:29 +0900

    fix: remove karabiner because it is unused

--- karabiner/karabiner-config-script/src/config/config_interface.ts
+++
@@ -1,5 +0,0 @@
-import type { ComplexModificationConfig } from "../karabiner/karabiner_types";
-
-export default interface ConfigInterface {
-  init(registerConfig: (c: ComplexModificationConfig) => void): void
-}
