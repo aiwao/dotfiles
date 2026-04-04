22c45bee6d7b732b6d6e340dccfadc7408bb6bb2
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-05 04:16:28 +0900

    Revert "input wezterm"
    This reverts commit 3f90ba6e21a0a0bf9ccd23be1ac17181c2c42729.

--- nix/cachix.nix
+++ nix/cachix.nix
@@ -4,7 +4,6 @@ let
     "https://cache.numtide.com"
     "https://devenv.cachix.org"
     "https://aiwao.cachix.org"
-    "https://wezterm.cachix.org"
   ];
   
   publicKeys = [
@@ -12,7 +11,6 @@ let
     "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "aiwao.cachix.org-1:b2LbtWNvJeL/qb1B6TYOMK+apaCps4SCbzlPRfSQIms="
-    "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
   ];
 in
 {
