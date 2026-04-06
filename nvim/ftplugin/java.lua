402a285eec33a7841a9ef200e4eb3d73795cf656
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-07 05:24:01 +0900

    feat: working jdtls config

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,66 @@
+local jdtls_bin_path = vim.fn.exepath("jdtls")
+if (vim.lsp.is_enabled("jdtls") or (jdtls_bin_path == "")) then
+  return
+end
+
+local jdtls = require("jdtls")
+jdtls.jol_path = os.getenv("NEOVIM_JOL_JAR")
+
+local config = vim.lsp.config["jdtls"]
+local cmd = {
+  os.getenv("JDK21") .. "/bin/java",
+  -- '-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044',
+  "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+  "-Dosgi.bundles.defaultStartLevel=4",
+  "-Declipse.product=org.eclipse.jdt.ls.core.product",
+  "-Dlog.protocol=true",
+  "-Dlog.level=ALL",
+  "-XX:+UseTransparentHugePages",
+  "-XX:+AlwaysPreTouch",
+  "-Xmx3g",
+  "--add-modules=ALL-SYSTEM",
+  "--add-opens", "java.base/java.util=ALL-UNNAMED",
+  "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+}
+config.cmd = cmd
+
+local jdtls_path = vim.fn.fnamemodify(jdtls_bin_path, ":h:h") .. "/share/java/jdtls/"
+local plugins_path = jdtls_path .. "plugins/"
+local home = os.getenv("HOME")
+local cache_path = home .. "/.cache/jdtls/"
+
+local launcher_path = vim.fn.glob(plugins_path .. "org.eclipse.equinox.launcher_*.jar")
+if vim.uv.fs_stat(launcher_path) then
+  local src_config_path = vim.uv.os_uname().sysname:lower() == "linux" and "config_linux/" or "config_mac/"
+  local config_path = cache_path .. src_config_path
+  vim.fn.mkdir(config_path, "p")
+  vim.uv.fs_copyfile(jdtls_path .. src_config_path .. "config.ini", config_path .. "config.ini")
+
+  vim.list_extend(cmd, {
+    "-jar", launcher_path,
+    "-configuration", config_path,
+  })
+else
+  vim.notify(launcher_path .. "is not found", vim.log.levels.ERROR)
+  return
+end
+
+local root_dir = config.root_dir or vim.fs.root(0, config.root_markers)
+local datadir
+if type(root_dir) == "string" then
+  datadir = cache_path .. "projects/" .. vim.fn.sha256(root_dir)
+  vim.fn.mkdir(datadir, "p")
+  table.insert(cmd, "-data")
+  table.insert(cmd, datadir)
+end
+
+local extendedClientCapabilities = jdtls.extendedClientCapabilities;
+extendedClientCapabilities.onCompletionItemSelectedCommand = "editor.action.triggerParameterHints"
+config.init_options = {
+  extendedClientCapabilities = extendedClientCapabilities;
+}
+-- mute; having progress reports is enough
+config.handlers = {
+  ["language/status"] = function() end
+}
+jdtls.start_or_attach(config)
--- nvim/lua/plugin/nvim-lspconfig.lua
+++ nvim/lua/plugin/nvim-lspconfig.lua
@@ -25,14 +25,75 @@ local config_list = {
   ["lemminx"] = {},
   ["kotlin_language_server"] = {},
   ["jdtls"] = {
+    root_markers = {
+      -- Multi-module projects
+      "mvnw", -- Maven
+      "gradlew", -- Gradle
+      "settings.gradle", -- Gradle
+      "settings.gradle.kts", -- Gradle
+      -- Use git directory as last resort for multi-module maven projects
+      -- In multi-module maven projects it is not really possible to determine what is the parent directory
+      -- and what is submodule directory. And jdtls does not break if the parent directory is at higher level than
+      -- actual parent pom.xml so propagating all the way to root git directory is fine
+      ".git",
+
+      -- Single-module projects
+      "build.xml", -- Ant
+      "pom.xml", -- Maven
+      "build.gradle", -- Gradle
+      "build.gradle.kts", -- Gradle
+    },
     settings = {
       java = {
+        autobuild = { enabled = false },
+        maxConcurrentBuilds = 8,
+        signatureHelp = { enabled = true };
+        contentProvider = { preferred = 'fernflower' };
+        saveActions = {
+          organizeImports = true,
+        },
+        completion = {
+          favoriteStaticMembers = {
+            "io.crate.testing.Asserts.assertThat",
+            "org.assertj.core.api.Assertions.assertThat",
+            "org.assertj.core.api.Assertions.assertThatThrownBy",
+            "org.assertj.core.api.Assertions.catchThrowable",
+            "java.util.Objects.requireNonNull",
+            "java.util.Objects.requireNonNullElse",
+            "org.mockito.Mockito.mock",
+            "org.mockito.Mockito.when",
+          },
+          filteredTypes = {
+            "com.sun.*",
+            "io.micrometer.shaded.*",
+            "java.awt.*",
+            "jdk.internal.*",
+            "sun.*",
+          },
+        };
+        sources = {
+          organizeImports = {
+            starThreshold = 9999;
+            staticStarThreshold = 9999;
+          };
+        };
+        codeGeneration = {
+          toString = {
+            template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+          },
+          hashCodeEquals = {
+            useJava7Objects = false,
+            useInstanceOf = true,
+          },
+          useBlocks = true,
+          addFinalForNewDeclaration = "fields",
+        };
         import = {
           gradle = {
-            enable = true
+            enabled = true
           },
           maven = {
-            enable = true
+            enabled = true
           },
         },
       },
@@ -66,7 +127,8 @@ for server, config in pairs(config_list) do
   config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
   vim.lsp.config(server, config)
   --rustaceanvim enables rust-analyzer manually
-  if not (server == "rust_analyzer") then
+  --nvim-jdtls enables jdtls manually
+  if not (server == "rust_analyzer" or server == "jdtls") then
     vim.lsp.enable(server)
   end
 end
