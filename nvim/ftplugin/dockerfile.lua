db6d14e914a90d7f34c8058ab521b2b4addb15b4
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-07 20:04:17 +0900

    refactor(neovim): move lsp-configs from `plugin/nvim-lspconfig.lua` to `nvim/ftplugin/`

---
+++ nvim/ftplugin/dockerfile.lua
@@ -0,0 +1,2 @@
+vim.lsp.enable("dockerls")
+vim.lsp.enable("docker_language_server")
--- nvim/lua/aiwao/plugin/nvim-lspconfig.lua
+++ nvim/lua/aiwao/plugin/nvim-lspconfig.lua
@@ -2,30 +2,6 @@ vim.pack.add {
   "https://github.com/neovim/nvim-lspconfig",
 }
 
-local config_list = {
-  ["lua_ls"] = {},
-  ["efm"] = {},
-  ["csharp_ls"] = {},
-  ["css_variables"] = {},
-  ["cssmodules_ls"] = {},
-  ["dockerls"] = {},
-  ["docker_language_server"] = {},
-  ["docker_compose_language_service"] = {},
-  ["eslint"] = {},
-  ["golangci_lint_ls"] = {},
-  ["gopls"] = {},
-  ["html"] = {},
-  ["sqlls"] = {},
-  ["svelte"] = {},
-  ["tailwindcss"] = {},
-  ["ts_ls"] = {},
-  ["basedpyright"] = {},
-  ["nixd"] = {},
-  ["vala_ls"] = {},
-  ["lemminx"] = {},
-  ["kotlin_language_server"] = {},
-}
-
 for server, config in pairs(require("aiwao.lsp.config")) do
   config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
   vim.lsp.config(server, config)
