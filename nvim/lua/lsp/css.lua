43ec2bc199c4540bfdc7787fd9f650949da5a557
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-07 18:44:55 +0900

    style(nvim): change structure style

--- nvim/lua/lsp/css.lua
+++ nvim/lua/lsp/css.lua
@@ -1,10 +1,10 @@
 ---@type LSPModule
-local M = {}
-
-function M.enable()
-  vim.lsp.enable("css_variables")
-  vim.lsp.enable("cssmodules_ls")
-  vim.lsp.enable("tailwindcss")
-end
+local M = {
+  enable = function()
+    vim.lsp.enable("css_variables")
+    vim.lsp.enable("cssmodules_ls")
+    vim.lsp.enable("tailwindcss")
+  end
+}
 
 return M
--- nvim/lua/lsp/web.lua
+++ nvim/lua/lsp/web.lua
@@ -1,11 +1,11 @@
 ---@type LSPModule
-local M = {}
-
-function M.enable()
-  vim.lsp.enable("eslint")
-  vim.lsp.enable("ts_ls")
-  vim.lsp.enable("html")
-  require("lsp.css").enable()
-end
+local M = {
+  enable = function()
+    vim.lsp.enable("eslint")
+    vim.lsp.enable("ts_ls")
+    vim.lsp.enable("html")
+    require("lsp.css").enable()
+  end,
+}
 
 return M
