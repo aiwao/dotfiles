f4cde87fbfeb4ea33852e11579d9073c5e326c48
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-09 04:28:13 +0900

    neovim(rustaceanvim): set rust-analyzer settigns to `vim.g.rustaceanvim.server.default_settings`

--- nvim/lua/aiwao/plugin/rustaceanvim.lua
+++ nvim/lua/aiwao/plugin/rustaceanvim.lua
@@ -1,9 +1,7 @@
 vim.pack.add { "https://github.com/mrcjkb/rustaceanvim" }
+
 vim.g.rustaceanvim = {
   server = {
-    default_settings = {
-      -- rust-analyzer language server configuration
-      require("aiwao.lsp.rust").config
-    },
+    default_settings = require("aiwao.lsp.rust").config or {},
   },
 }
