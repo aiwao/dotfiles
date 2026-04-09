456ef2558616f220f47ff6ffc06c18652df52906
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-09 12:46:48 +0900

    system(linux): add subuids, subgids

--- nix/modules/linux/system.nix
+++ nix/modules/linux/system.nix
@@ -2,5 +2,12 @@
   username,
   ...
 }: {
-  
+  environment.etc = {
+    subuid.text = ''
+      ${username}:100000:65536
+    '';
+    subgid.text = ''
+      ${username}:100000:65536
+    '';
+  };
 }
