828c238ea3595ba27a6d3e6df4b4dabc25a7c5aa
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-09 13:21:22 +0900

    fix(linux/system): write subuid, subgid when file existing

--- nix/modules/linux/system.nix
+++ nix/modules/linux/system.nix
@@ -3,11 +3,17 @@
   ...
 }: {
   environment.etc = {
-    subuid.text = ''
-      ${username}:100000:65536
-    '';
-    subgid.text = ''
-      ${username}:100000:65536
-    '';
+    subuid = {
+      text = ''
+        ${username}:100000:65536
+      '';
+      replaceExisting = true;
+    };
+    subgid = {
+      text = ''
+        ${username}:100000:65536
+      '';
+      replaceExisting = true;
+    };
   };
 }
