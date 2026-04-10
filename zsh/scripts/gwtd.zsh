e45700abaf493fa5d509cfd3d768bf95be8ad02e
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-11 07:16:57 +0900

    feat(zsh): Usage: gwtd <branch>(git-wt delete) script

--- zsh/scripts/gwtd.zsh
+++ zsh/scripts/gwtd.zsh
@@ -3,6 +3,32 @@ gwtd() {
     echo "Usage: gwtd <branch>"
     return 1
   fi
+  local target_branch=$1
 
-  local root="$(git_root)" || return 1
+  local wt_path="$(git worktree list | awk -v b=$target_branch "\$0 ~ b { print \$1 }")" || return 1
+  echo $wt_path
+  if [[ ! -e $wt_path ]] then
+    echo "Failed to locale worktree dir"
+    return 1
+  fi
+  
+  git-wt -d $target_branch || return 1
+  
+  echo -n "Remove a workspace? (y/n): "
+  read rm_work
+  case $rm_work in
+    [Yy]*) rm -rf $wt_path ;;
+    [Nn]*) exit 0 ;;
+    *)     echo "Please answer y or n" >&2; exit 1 ;;
+  esac
+
+  if git branch -r | grep -q "^ *origin/$target_branch\$"; then
+    echo -n "Remove a remote branch? (y/n): "
+    read rm_rem_br
+    case $rm_rem_br in
+      [Yy]*) git push origin --delete $target_branch ;;
+      [Nn]*) exit 0 ;;
+      *)     echo "Please answer y or n" >&2; exit 1 ;;
+    esac
+  fi
 }
