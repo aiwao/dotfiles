016a5774e5879fdc1cca95326f15ba1e2b60a627
aiwao <233898056+aiwao@users.noreply.github.com>
2026-04-16 03:14:17 +0900

    zsh:ghqn: now pre set query with arg

--- zsh/scripts/ghqn.zsh
+++ zsh/scripts/ghqn.zsh
@@ -1,5 +1,10 @@
 ghqn() {
-  local selected=$(ghq list --full-path | roots | fzf --height 40% --reverse)
+  local query=""
+  if [[ $# -gt 0 ]]; then
+    query="--query=$@"
+  fi
+
+  local selected=$(ghq list --full-path | roots | fzf $query --height 40% --reverse)
   if [[ -e "$selected" ]] then
     cd "$selected"
   fi
