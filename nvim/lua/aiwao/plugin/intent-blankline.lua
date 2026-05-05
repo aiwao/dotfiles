4ac666984f22b8363d7b63a6251c670c6d51862f
aiwao <233898056+aiwao@users.noreply.github.com>
2026-05-05 16:43:06 +0900

    neovim(ibl): remove underline, smart_indent_cap

--- nvim/lua/aiwao/plugin/intent-blankline.lua
+++ nvim/lua/aiwao/plugin/intent-blankline.lua
@@ -2,4 +2,12 @@ vim.pack.add {
   "https://github.com/lukas-reineke/indent-blankline.nvim",
   "https://github.com/TheGLander/indent-rainbowline.nvim"
 }
-require("ibl").setup(require("indent-rainbowline").make_opts({}))
+require("ibl").setup(require("indent-rainbowline").make_opts({
+  indent = {
+    smart_indent_cap = false,
+  },
+  scope = {
+    show_start = false,
+    show_end = false,
+  },
+}))
