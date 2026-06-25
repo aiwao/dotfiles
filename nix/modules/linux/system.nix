1e6c111e81439bb2cc26a3caa877912ec2c439bf
aiwao <233898056+aiwao@users.noreply.github.com>
2026-06-25 11:19:26 +0900

    feat(linux): add mullvad daemon service

--- nix/modules/linux/system.nix
+++ nix/modules/linux/system.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   username,
   ...
 }: {
@@ -14,6 +15,29 @@
         ${username}:100000:65536
       '';
       replaceExisting = true;
+    };
+  };
+
+  systemd.services.mullvad-daemon = {
+    enable = true;
+    description = "Mullvad VPN daemon";
+    wantedBy = [ "multi-user.target" ];
+    wants = [
+      "network.target"
+      "network-online.target"
+    ];
+    after = [
+      "network-online.target"
+      "NetworkManager.service"
+      "systemd-resolved.service"
+    ];
+    startLimitBurst = 5;
+    startLimitIntervalSec = 20;
+    serviceConfig = {
+      ExecStartPre = "-${pkgs.kmod}/bin/modprobe tun";
+      ExecStart = "${pkgs.mullvad}/bin/mullvad-daemon -v --disable-stdout-timestamps";
+      Restart = "always";
+      RestartSec = 1;
     };
   };
 }
